5a53bea6f79b05e03056ae740f513bbda980e799
Emilio <lemigliot@gmail.com>
2023-11-08 16:08:00 +0100

    esercizi primo modulo

---
+++ destru4.js
@@ -0,0 +1,24 @@
+//Date due variabili a e b, scambia i loro valori usando la destrutturazione.
+
+let a = 5;
+let b = 10;
+
+let array = [a, b]
+
+
+
+//const [x, y] = array
+
+x = array[1]
+
+y = array[0]
+
+const [x, y] = array
+
+
+
+
+
+
+
+console.log(x);
